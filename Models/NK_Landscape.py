294ad33d18e6f2cb0125ed0da6837701bc41d351
조한슬 <jhs4015@kaist.ac.kr>
2021-08-24 15:49:46 +0900

    Function based code

--- Models/NK_Landscape.py
+++ Models/NK_Landscape.py
@@ -1,6 +1,18 @@
+"""
+Function version of NKmodel
+"""
+
 import random
 import numpy as np
 import itertools
+
+def binarr2int(arr):
+    # Example:
+    # np.array([0 1 1 0 1 0]) -> 26
+    return int(arr.dot(1 << np.arange(arr.size)[::-1]))
+
+def int2binarr(num, width):
+    return np.array([int(x) for x in np.binary_repr(num, width=width)])
 
 def dependency(N, K):  # interdependency matrix
     interdependence = (np.eye(N, dtype=float) == 1)
@@ -10,36 +22,32 @@ def dependency(N, K):  # interdependency matrix
     return interdependence
 
 def contri(N, K):  # contribution map
-    contributions = [{} for _ in range(N)]
-    for i in range(N):
-        for label in itertools.product(range(2), repeat=K+1):  # K+1 subcollection of loci values that effects the locus i
-            contributions[i][label] = random.random()  # float [0, 1]
-    return contributions
+    return np.random.random(size=(N,2**(K+1)))  # floats [0, 1]
 
 def _calculate_ith_contribution(state, ctrb_map, inter_mat, i):
     interdep = inter_mat[i].copy()
     interdep[i] = False
-    label = tuple([state[i]] + list(state[interdep]))  # the value of i-th locus should be the first entry of the 'label'.
-    return ctrb_map[i][label]
+    label = np.append(state[i], state[interdep])  # the value of i-th locus should be the first entry of the 'label'.
+    return ctrb_map[i][binarr2int(label)]
 
 def fitness(state, ctrb_map, inter_mat, negative=False):
     """
-    Given a state(: a tuple/array of length N), 
+    Given a state(: an array of length N), 
     Return fitness value followed by associated contributions.
     """
-    state = np.array(state).flatten()
+    state = state.flatten()
     N = state.size
     ctrbs = [_calculate_ith_contribution(state, ctrb_map, inter_mat, i) for i in range(N)]
     fitness_value = sum(ctrbs) / N  # averaged fitness value --> btw 0 ~ 1
     if negative:
         fitness_value = -fitness_value
-    return ctrbs + fitness_value
+    return ctrbs + [fitness_value]  # payoff
 
-def NK_landscape(N, K, negative=False):
+def NK_landscape(N=6, K=1, negative=False):
     inter_mat = dependency(N, K)
     ctrb_map = contri(N, K)
-    
     states = itertools.product(range(2), repeat=N)
-    landscape_dic = {state: fitness(state, ctrb_map, inter_mat, negative=negative) for state in states}
-
+    landscape = [fitness(np.array(state), ctrb_map, inter_mat, negative=negative) for state in states]
+    landscape = np.array(landscape)
+    return inter_mat, ctrb_map, landscape
 
